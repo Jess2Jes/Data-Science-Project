9b5b201f68fb90da90130c73f2e8b1a57fdb3ac2
Milkdrinker-creator <kindylim07@gmail.com>
2025-10-04 16:11:35 +0700

    Update Program Pencarian & Perubahan Data.py

--- helpers/Program Pencarian & Perubahan Data.py
+++ helpers/Program Pencarian & Perubahan Data.py
@@ -1,10 +1,19 @@
-#kindy
-#cari
-my_list = [1, 2, 3, 4, 5]
-cari=input("Cari angka :")
-if cari in my_list:
-    print(f"{cari} is present in the list")
-else:
-    print(f"{cari} is not present in the list")
+#contoh cari
+my_list = ["apel", "air", "ikan", "monyet", "sapi"]
+print(my_list)
+cari=input("Cari data :")
 
-#ubah
+for index, data in enumerate(my_list):
+    print(f"Index: {index}, data: {data}/n")
+if cari in my_list:
+    print(f"{cari} is present in the list/n")
+else:
+    print(f"{cari} is not present in the list/n")
+
+#contoh ubah
+print(my_list)
+pilih=input("Pilih data yang mau diubah :")
+new=input("Diubah menjadi : ")
+replace = my_list.index(pilih)
+my_list[replace] =  new
+print(my_list)
