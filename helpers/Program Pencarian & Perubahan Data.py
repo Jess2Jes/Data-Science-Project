e5439c5ba663c0c96b8b221e07c2d8a3dfb0672a
Milkdrinker-creator <kindylim07@gmail.com>
2025-10-04 15:53:32 +0700

    Update Program Pencarian & Perubahan Data.py

--- helpers/Program Pencarian & Perubahan Data.py
+++ helpers/Program Pencarian & Perubahan Data.py
@@ -1,1 +1,10 @@
 #kindy
+#cari
+my_list = [1, 2, 3, 4, 5]
+cari=input("Cari angka :")
+if cari in my_list:
+    print(f"{cari} is present in the list")
+else:
+    print(f"{cari} is not present in the list")
+
+#ubah
