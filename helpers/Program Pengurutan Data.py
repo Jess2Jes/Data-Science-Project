e562da00adfc7c4f6f20f02838ac0fc0edd3b0df
Queen of the Underworld <167067576+Jess2Jes@users.noreply.github.com>
2025-10-04 21:30:13 +0700

    Update Program Pengurutan Data.py Penambahan Kode

--- helpers/Program Pengurutan Data.py
+++ helpers/Program Pengurutan Data.py
@@ -1,1 +1,36 @@
-#jessica
+# Pengurutan Data 
+import datetime
+from datetime import *
+
+# Contoh data: 
+datas = [("tidur", f"{datetime.now().date()} 22:00"), 
+        ("mengerjakan kuis matkul Data Science", f"{datetime.now().date()} 08:30"), 
+        ("sarapan", f"{datetime.now().date()} 08:00"), 
+        ("jogging", f"{datetime.now().date()} 07:30"),
+        ("menulis laporan pengerjaan project OOP", f"{datetime.now().date()} 09:00"),
+        ("mengikuti kelas Data Science", f"{datetime.now().date()} 17:45")]
+
+# Cetak data sebelum disortir
+def cetak_data_unsorted(datas):
+    print("\n" + "Daftar Task/Tugas Sebelum Disortir".center(80, '-')
+        + "\n")
+    for idx, data in enumerate(datas,start=1):
+        print(f"-> Tugas-{idx}: {data[0].title()} ({data[1]})")
+    print("\n" + "-"*80)
+
+# Proses pengurutan data
+def urutkan_task(datas):
+    sort_datas = sorted(datas, key=lambda x: x[1])
+    return sort_datas
+
+# Cetak data setelah disortir
+def cetak_data_sorted(datas):
+    print("\n" + "Daftar Task/Tugas Setelah Disortir".center(80, '-')
+        + "\n")
+    for idx, data in enumerate(datas,start=1):
+        print(f"-> Tugas-{idx}: {data[0].title()} ({data[1]})")
+    print("\n" + "-"*80)
+
+cetak_data_unsorted(datas)
+new_datas = urutkan_task(datas)
+cetak_data_sorted(new_datas)#jessica
