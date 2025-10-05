be03d8a385d4c7b8ca8028d77ec4a2623620d4a9
Hans 展豪 <hansvalerie19@gmail.com>
2025-10-05 15:25:07 +0700

    Update sorter.py

--- helpers/sorter.py
+++ helpers/sorter.py
@@ -1,14 +1,3 @@
-# Pengurutan Data 
-
-# Contoh data: 
-tasks = [
-    {"id": 1, "name": "Belajar Algoritma", "deadline": "2025-10-06 12:57 PM", "priority": "Medium", "status": "In Progress"},
-    {"id": 2, "name": "Kerjakan Tugas Basis Data", "deadline": "2025-10-05 13:00 PM", "priority": "High", "status": "Pending"},
-    {"id": 3, "name": "Buat Slide Presentasi", "deadline": "2025-10-08 16:49 PM", "priority": "High", "status": "Completed"},
-    {"id": 4, "name": "Rapat Tim", "deadline": "2025-10-04 15:00 PM", "priority": "Low", "status": "Completed"},
-    {"id": 5, "name": "Kerjakan Tugas PBO", "deadline": "2025-10-04 09:00 AM", "priority": "High", "status": "Completed"},
-]
-
 # Proses pengurutan data berdasarkan deadline (ascending)
 def urutkan_task_deadline_asc(tasks):
     tasks.sort(key=lambda x: x["deadline"])
